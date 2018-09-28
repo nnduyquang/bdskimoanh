5c478d4c1b4af10c52908381aa62aad8a4d3a8af
loiPH <loiph.vncoder@users.noreply.github.com>
2018-09-28 17:22:15 +0700

    update thêm phần chi tiết

--- local/resources/views/frontend/tuyendung-details/index.blade.php
+++ local/resources/views/frontend/tuyendung-details/index.blade.php
@@ -24,12 +24,12 @@
     <div class="container-fluid mt-4">
         <div class="container">
             <div class="row">
-                <div class="col-md-8" id="">
+                <div class="col-md-8 p-lg-1 p-0" id="">
                     @include('frontend.tuyendung-details.nd_2')
                     @include('frontend.tuyendung-details.nd_1')
                 </div>
 
-                <div class="col-md-4">
+                <div class="col-md-4 p-lg-1 p-0">
                     @include('frontend.common.r_n_n')
                 </div>
             </div>
